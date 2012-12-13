33aa012e1ebfb0287228215f2cc0edee1c1f88e3
Matthew Schrager <matthewschrager@gmail.com>
2012-12-12 21:57:16 -0500

    Moved tests to separate project.

--- Repository.EntityFramework/EFRepository.cs
+++ Repository.EntityFramework/EFRepository.cs
@@ -206,37 +206,4 @@ namespace Repository.EntityFramework
         }
         //===============================================================
     }
-
-    internal class TestObject
-    {
-        //===============================================================
-        [Key]
-        public int ID { get; set; }
-        //===============================================================
-        public String Value { get; set; }
-        //===============================================================
-    }
-
-    internal class TestContext : DbContext
-    {
-        //===============================================================
-        public DbSet<TestObject> Objects { get; set; }
-        //===============================================================
-    }
-
-    [TestFixture]
-    internal class EFRepositoryTests
-    {
-        //===============================================================
-        [Test]
-        public void BatchInsertTest()
-        {
-            using (var repo = new EFRepository<TestContext, TestObject>(x => x.Objects, x => x.ID))
-            {
-                var objects = Enumerable.Range(0, 100).Select(x => new TestObject { ID = x, Value = x.ToString() }).ToList();
-                repo.Store(objects);
-            }
-        }
-        //===============================================================
-    }
 }
--- Repository.Tests/InMemoryRepositoryTest.cs
+++ Repository.Tests/InMemoryRepositoryTest.cs
@@ -1,29 +1,30 @@
 ﻿using System.Dynamic;
+using NUnit.Framework;
 using Repository;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 namespace Repository.Tests
 {
-    
-    
+
+
     /// <summary>
     ///This is a test class for InMemoryRepositoryTest and is intended
     ///to contain all InMemoryRepositoryTest Unit Tests
     ///</summary>
-    [TestClass()]
-    public class InMemoryRepositoryTest
+    [TestFixture]
+    internal class InMemoryRepositoryTest
     {
 
 
-        private TestContext testContextInstance;
+        private NUnit.Framework.TestContext testContextInstance;
 
         /// <summary>
         ///Gets or sets the test context which provides
         ///information about and functionality for the current test run.
         ///</summary>
-        public TestContext TestContext
+        public NUnit.Framework.TestContext TestContext
         {
             get
             {
@@ -35,50 +36,21 @@ namespace Repository.Tests
             }
         }
 
-        #region Additional test attributes
-        // 
-        //You can use the following additional attributes as you write your tests:
-        //
-        //Use ClassInitialize to run code before running the first test in the class
-        //[ClassInitialize()]
-        //public static void MyClassInitialize(TestContext testContext)
-        //{
-        //}
-        //
-        //Use ClassCleanup to run code after all tests in a class have run
-        //[ClassCleanup()]
-        //public static void MyClassCleanup()
-        //{
-        //}
-        //
-        //Use TestInitialize to run code before running each test
-        //[TestInitialize()]
-        //public void MyTestInitialize()
-        //{
-        //}
-        //
-        //Use TestCleanup to run code after each test has run
-        //[TestCleanup()]
-        //public void MyTestCleanup()
-        //{
-        //}
-        //
-        #endregion
 
-        [TestMethod()]
+        [Test]
         public void UpdateTest()
         {
-            var repository = new InMemoryRepository<TestClass>(x => x.Key);
-            repository.Store(new TestClass());
+            var repository = new InMemoryRepository<Repository.TestClass>(x => x.Key);
+            repository.Store(new Repository.TestClass());
             repository.Update(new { Value2 = DateTime.MaxValue }, 1);
 
             var val = repository.Find(1).Object;
-            Assert.AreEqual(val.Value2, DateTime.MaxValue);
+            NUnit.Framework.Assert.AreEqual(val.Value2, DateTime.MaxValue);
 
             var obj = new { Value2 = DateTime.MaxValue };
             repository.Update(obj, x => x.Property, 1);
-            Assert.AreEqual(val.Property.Value2, DateTime.MaxValue);
-            Assert.AreEqual(val.Property.Value1.Value, 1);
+            NUnit.Framework.Assert.AreEqual(val.Property.Value2, DateTime.MaxValue);
+            NUnit.Framework.Assert.AreEqual(val.Property.Value1.Value, 1);
 
         }
     }
